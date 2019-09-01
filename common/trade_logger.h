8058b61ba7dbffa19daf38735497d40118f29a49
zc8424 <zc8424@126.com>
2019-09-01 22:59:21 +0800

    Insert trade record in "records" table.

--- common/trade_logger.h
+++ common/trade_logger.h
@@ -18,6 +18,7 @@ public:
     void closeShort(qint64 actionTime, const QString &instrumentID, double price, int volume);
 
 protected:
+    void addRecord();
     void saveActionToDB(qint64 actionTime, const QString &instrumentID, double price, int volume, bool direction, bool openCloseFlag);
 };
 
